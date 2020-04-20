57f5cbbe4c008fd808f8fef54be15c85acaec017
Sman Hman <shlomihelfgot@gmail.com>
2020-04-20 17:45:16 +0300

    clock

--- client-side.js
+++ client-side.js
@@ -86,39 +86,12 @@ peerConnection = new RTCPeerConnection(peerConnectionConfig);
         //Then, once the submit button on the explanatory overlay is clicked, time to completely make over the entire page...
         $(document).on('click','.start',function(){
           //set clock
-          setInterval( function() {
-              var hours = new Date().getHours();
-              var mins = new Date().getMinutes();
-              var hdegree = hours * 30 + (mins / 2);
-              var hrotate = "rotate(" + hdegree + "deg)";
-              
-              $(".hand").css("opacity","1");
-              
-              $("#hour").css({
-                "-webkit-transform" : hrotate,
-                "-moz-transform" : hrotate,
-                "-ms-transform" : hrotate,
-                "-o-transform" : hrotate,
-                "transform" : hrotate
-              });
-              
-            }, 1000 );
-      
-            setInterval( function() {
-              var mins = new Date().getMinutes();
-              var mdegree = mins * 6;
-              var mrotate = "rotate(" + mdegree + "deg)";
-              
-              $("#minute").css({
-                "-webkit-transform" : mrotate,
-                "-moz-transform" : mrotate,
-                "-ms-transform" : mrotate,
-                "-o-transform" : mrotate,
-                "transform" : mrotate
-              });
-              
-            }, 1000 );
-          
+           var sec = 0;
+          function pad ( val ) { return val > 9 ? val : "0" + val; }
+          setInterval( function(){
+              $("#seconds").html(pad(++sec%60));
+              $("#minutes").html(pad(parseInt(sec/60,10)));
+          }, 1000);
           //get the email and screen name
           var screenName = $('#screen_name').val();
           var email = $('#email').val();
